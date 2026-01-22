21c5533c46244bdee5d0f91b2fa8f8a7a7f91381
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-01-22 09:33:55 -0300

    feat: añadir ejercicio interfaces avanzadas

---
+++ ejercicios/00-introduccion/25-interfaces-avanzadas.go
@@ -0,0 +1,37 @@
+// Ejemplo avanzado de interfaces en Go
+package main
+
+import "fmt"
+
+type Forma interface {
+	Area() float64
+}
+
+type Circulo struct {
+	Radio float64
+}
+
+type Rectangulo struct {
+	Ancho, Alto float64
+}
+
+func (c Circulo) Area() float64 {
+	return 3.14 * c.Radio * c.Radio
+}
+
+func (r Rectangulo) Area() float64 {
+	return r.Ancho * r.Alto
+}
+}
+
+func imprimirArea(f Forma) {
+	fmt.Println("Área:", f.Area())
+}
+
+func main() {
+	c := Circulo{Radio: 5}
+	r := Rectangulo{Ancho: 4, Alto: 6}
+
+	imprimirArea(c)
+	imprimirArea(r)
+}
